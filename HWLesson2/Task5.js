ddf5da20a27cff8b79d99aeb425403446540e404
semenb <64479873+semenb@users.noreply.github.com>
2020-05-06 11:53:56 +0300

    Update HWLesson2/Task5.js
    Co-authored-by: Inna Ivashchuk <innes333@gmail.com>

--- HWLesson2/Task5.js
+++ HWLesson2/Task5.js
@@ -1,4 +1,11 @@
 const checkForSpam = function(message) {
+   if (message.toLowerCase().includes('spam')) {
+     return true;
+   }
+  
+  // dafault value
+  return false;
+}
   let check = false;
   if (message.toLowerCase().includes('spam')) {
     check = true;
